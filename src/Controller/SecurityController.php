299a588d29ed2106f32461caadbbbdb349f29050
Kaoutiz <94445745+Kaoutiz@users.noreply.github.com>
2023-12-21 14:18:18 +0100

    bryan-branch-2: Redirect to admin after login

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -14,7 +14,7 @@ class SecurityController extends AbstractController
     {
         
         if ($this->getUser()) {
-             return $this->redirectToRoute('app_dashboard');
+             return $this->redirectToRoute('admin');
         }
 
         // get the login error if there is one
