b1b6b2db46d8a53c3f0878d53f8f31f37e0d2f5a
Maxime Canac <maxime.canac@etu.unilim.fr>
2023-12-24 01:03:43 +0100

    dashboard-admin-branch : add Admin CRUD for link, update dashboard for link , modify managed user in dashboard

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -3,26 +3,69 @@
 namespace App\Controller\Admin;
 
 use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
-use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class UserCrudController extends AbstractCrudController
 {
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     public static function getEntityFqcn(): string
     {
         return User::class;
     }
+    public function configureActions(Actions $actions): Actions
+    {
+        $actions->disable(Action::NEW);
 
-    /*
+        return $actions;
+    }
+
     public function configureFields(string $pageName): iterable
     {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
+        $fields = parent::configureFields($pageName);
+
+        // Ajouter le champ de mot de passe uniquement pour la page d'édition
+        if (Crud::PAGE_EDIT === $pageName) {
+            $fields[] = TextField::new('password')
+                ->setLabel('Nouveau mot de passe')
+                ->setFormType(\Symfony\Component\Form\Extension\Core\Type\PasswordType::class)
+                ->setHelp('');
+        }
+
+        return $fields;
     }
-    */
+
+    public function updateEntity(EntityManagerInterface $entityManager, $entityInstance): void
+    {
+        if (!$entityInstance instanceof User) {
+            return;
+        }
+
+        $plainPassword = $entityInstance->getPassword();
+
+        if (!empty($plainPassword)) {
+            $hashedPassword = $this->encodePassword($entityInstance, $plainPassword);
+            $entityInstance->setPassword($hashedPassword);
+        }
+
+        parent::updateEntity($entityManager, $entityInstance);
+    }
+
+    private function encodePassword(User $user, string $plainPassword): string
+    {
+        // Remplacez cette logique par l'encodage de mot de passe approprié
+        return password_hash($plainPassword, PASSWORD_DEFAULT);
+    }
 }
+
+
